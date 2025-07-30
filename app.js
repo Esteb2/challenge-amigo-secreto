bc5a239c6ef3b53842298e718fb67f37d6d67687
Esteb2 <190539635+Esteb2@users.noreply.github.com>
2025-07-30 15:24:35 -0500

    feat: implement random name selector

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const nameInput = document.getElementById("amigo");
+const friendList = document.getElementById("listaAmigos");
+const choosenOne = document.getElementById("resultado");
+
+let friends = [];
+
+function addFriend() {
+  const name = nameInput.value.trim();
+  if (name) {
+    friends.push(name);
+    alert(`Amigo añadido: ${name}`);
+    nameInput.value = "";
+  } else {
+    alert("Por favor, inserte un nombre.");
+  }
+  displayFriends();
+}
+
+function displayFriends() {
+  friendList.innerHTML = "";
+  friends.forEach((friend) => {
+    const li = document.createElement("li");
+    li.textContent = friend;
+    friendList.appendChild(li);
+  });
+}
+
+function drawFriend() {
+  if (friends.length === 0) {
+    alert("No hay amigos para sortear.");
+    return;
+  }
+  const randomIndex = Math.floor(Math.random() * friends.length);
+  const selectedFriend = friends[randomIndex];
+  choosenOne.textContent = `Tu amigo secreto es: ${selectedFriend}`;
+}
